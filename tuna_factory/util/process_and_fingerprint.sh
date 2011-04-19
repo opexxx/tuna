adf2e2032521112bc8fe12a77b91606018f5cad9
jcran <jcran@0x0e.org>
2011-04-18 22:50:11 -0500

    more fingerprinting

---
+++ tuna_factory/util/process_and_fingerprint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+EXPECTED_ARGS=1
+
+if [ $# -ne $EXPECTED_ARGS ]
+then
+  echo "Usage: `basename $0` {dir}"
+  exit 99
+fi
+
+DIR=$1
+echo "Tuna directory: $DIR"
+
+rake tuna:process_and_fingerprint TUNA_DIR=$DIR
